3a6ac0505f8fec1db514a3e8fbe5a7cc542cf118
Stas Morozevich <s.morozevich@gpsolutions.com>
2019-07-09 19:32:18 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import Link from "next/link";
+import Layout from "../components/Layout";
+import { NextPage } from "next";
+import { gql } from "apollo-boost";
+
+import { Query } from "react-apollo";
+
+const IndexPage: NextPage = () => {
+  return (
+    <Query query={gql`
+      { viewer { login }}
+    `}>
+      {({ data } : any) => {
+        console.log("IndexPage data:", data);
+
+        return (
+          <Layout title="Home | Next.js + TypeScript Example">
+            <h1>hello Next.js 👋</h1>
+            <p>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </p>
+          </Layout>
+        );
+      }}
+    </Query>
+  );
+};
+
+export default IndexPage;
