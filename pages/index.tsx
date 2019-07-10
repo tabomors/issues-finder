fca255c55efdbeeb076f50400ac4892e50944a09
Stas Morozevich <s.morozevich@gpsolutions.com>
2019-07-10 20:01:38 +0300

    Render edges

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,31 @@
-import * as React from "react";
+import React from "react";
 import Link from "next/link";
 import Layout from "../components/Layout";
 import { NextPage } from "next";
-import { useFindIssuesQuery } from "../generated/graphql";
+import { useFindIssuesQuery, FindIssuesQuery } from "../generated/graphql";
+import get from 'lodash/get';
 
 const IndexPage: NextPage = () => {
-  const issuesData = useFindIssuesQuery({
+  const { data } = useFindIssuesQuery({
     variables: { query: "language:javascript label:good-first-issue" }
   });
-  console.log(issuesData.data)
-
+  
+  const edges = get(data, 'search.edges', [])
+  
   return (
     <Layout title="Home | Next.js + TypeScript Example">
-      <h1>hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
+      {/* <h1>hello Next.js 👋</h1> */}
+      <div>
+        {edges.map((edge: any) => {
+          return <pre key={edge.node.id}>{JSON.stringify(edge, null, '\t')}</pre>
+        })}
+
+        {/* <Link href="/about">
           <a>About</a>
-        </Link>
-      </p>
+        </Link> */}
+
+        
+      </div>
     </Layout>
   );
 };
