f35413f095a4800e4b0928abd55b106bc43d5d6c
Stas Morozevich <s.morozevich@gpsolutions.com>
2019-07-11 13:54:23 +0300

    Add loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,28 +3,31 @@ import Link from "next/link";
 import Layout from "../components/Layout";
 import { NextPage } from "next";
 import { useFindIssuesQuery, FindIssuesQuery } from "../generated/graphql";
-import get from 'lodash/get';
+import get from "lodash/get";
 
 const IndexPage: NextPage = () => {
-  const { data } = useFindIssuesQuery({
+  const { data, loading } = useFindIssuesQuery({
     variables: { query: "language:javascript label:good-first-issue" }
   });
-  
-  const edges = get(data, 'search.edges', [])
-  
+
+  const edges = get(data, "search.edges", []);
+
   return (
     <Layout title="Home | Next.js + TypeScript Example">
-      {/* <h1>hello Next.js 👋</h1> */}
       <div>
-        {edges.map((edge: any) => {
-          return <pre key={edge.node.id}>{JSON.stringify(edge, null, '\t')}</pre>
-        })}
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          edges.map((edge: any) => {
+            return (
+              <pre key={edge.node.id}>{JSON.stringify(edge, null, "\t")}</pre>
+            );
+          })
+        )}
 
         {/* <Link href="/about">
           <a>About</a>
         </Link> */}
-
-        
       </div>
     </Layout>
   );
