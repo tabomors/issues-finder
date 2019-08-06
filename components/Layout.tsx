16a326480711b16641cf59b7ed01c4bffcbdae35
Stas Morozevich <stas.morozevich@gmail.com>
2019-08-06 23:34:39 +0300

    Remove unused code

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -20,14 +20,6 @@ const Layout: React.FunctionComponent<Props> = ({
       <nav>
         <Link href="/">
           <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/initial-props">
-          <a>With Initial Props</a>
         </Link>
       </nav>
     </header>
