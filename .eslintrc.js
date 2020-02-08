a7ad99da4d148aecef3194ad8ecffbe3c29c887a
Stas Morozevich <stas.morozevich@gmail.com>
2020-02-09 00:10:54 +0300

    Move to eslint from tslint

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    '@typescript-eslint',
+    'react-hooks',
+    'jsx-a11y'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended'
+  ],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': [
+      // It forces you to add "I" prefix for interfaces
+      'warn',
+      { prefixWithI: 'always' }
+    ],
+    'react/react-in-jsx-scope': 'off', // Without it, you will get errors since NextJs does not require you to import React into each component
+    'react/prop-types': 'off',
+    'react/prefer-stateless-function': 1,
+    'react-hooks/rules-of-hooks': 'error', // https://reactjs.org/docs/hooks-rules.html
+    'react-hooks/exhaustive-deps': 'warn', // https://reactjs.org/docs/hooks-rules.html
+    'prefer-object-spread': 'error', // Prefer use of an object spread over Object.assign
+    'jsx-a11y/anchor-is-valid': 'warn' // Warn because we have RouterLink which passes href to child link
+  },
+  settings: {
+    react: {
+      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  }
+};
